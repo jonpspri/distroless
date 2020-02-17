6672e43e421f78a0564f771000792c99d0e602d3
Jonathan Springer <jonpspri@gmail.com>
2020-02-17 13:17:43 -0800

    Switch to build script to build.

---
+++ jenkins_build.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env sh
+
+registry=${OPENWHISK_TARGET_REGISTRY:-docker.io}
+prefix=${OPENWHISK_TARGET_PREFIX:-s390xopenwhisk}
+
+mkdir -p ${WORKSPACE}/${target}/.docker
+cp -R ${HOME}/.docker/* ${WORKSPACE}/${target}/.docker
+rm -rf ${WORKSPACE}/${target}/.docker/manifests
+
+for target in \
+    base:base_debian9 base:base_debian10 \
+    base:static_debian9 base:static_debian10
+do (
+    # Fully qualified image (to make our script more terse)
+    fq_image=${registry}/${prefix}/${target}
+
+    for arch in amd64 arm64v8 ppc64le s390x; do
+      bazel run --host_force_python=PY2 "//${target}_${arch}"
+      docker tag bazel/${target}_${arch} ${fq_image}_${arch}
+      docker push ${fq_image}_${arch}
+    done
+
+    docker --config ${WORKSPACE}/${target}/.docker \
+      manifest create ${fq_image} \
+          ${fq_image}_amd64 \
+          ${fq_image}_arm64v8 \
+          ${fq_image}_ppc64le \
+          ${fq_image}_s390x
+
+    for arch in amd64 arm64v8 ppc64le s390x; do
+      docker --config ${WORKSPACE}/${target}/.docker \
+        manifest annotate --os linux --arch ${arch} \
+        ${fq_image} ${fq_image}_${arch}
+    done
+
+    docker --config ${WORKSPACE}/${target}/.docker \
+      manifest push ${fq_image}
+) done
