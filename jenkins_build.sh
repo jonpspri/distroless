91178626239fd0797e90c2a77576a4940a747dc5
Jonathan Springer <jonpspri@gmail.com>
2020-02-21 11:18:26 -0800

    Add build of cc multi-architecture (Take 1 - not elegant).

--- jenkins_build.sh
+++ jenkins_build.sh
@@ -12,7 +12,8 @@ for target in \
     base:base_debian9 base:base_debian10 \
     base:base-nonroot_debian9 base:base-nonroot_debian10 \
     base:static_debian9 base:static_debian10 \
-    base:static-nonroot_debian9 base:static-nonroot_debian10
+    base:static-nonroot_debian9 base:static-nonroot_debian10 \
+    cc:cc_debian9 cc:cc_debian10
 do (
     # Fully qualified image (to make our script more terse)
     fq_image=${registry}/${prefix}/${name_prefix}${target}
